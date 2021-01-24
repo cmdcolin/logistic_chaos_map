28bfe037ce7c672a066c0f01481fd2c2f3cce8c0
Colin <colin.diesh@gmail.com>
2021-01-23 19:34:57 -0700

    Move files around

--- workspace-b/src/App.js
+++ workspace-b/src/App.js
@@ -49,7 +49,7 @@ function App() {
   useEffect(() => {
     (async () => {
       try {
-        const wasm = await import("logistic_map_wasm");
+        const wasm = await import("workspace-a");
         setWasm(wasm);
       } catch (e) {
         console.error(e);
