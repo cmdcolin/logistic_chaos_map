959b0a62836de3bfdfde8a61450a59d68418cf33
Colin <colin.diesh@gmail.com>
2025-06-16 13:59:19 -0400

    Updates

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
 import react from '@vitejs/plugin-react'
-import topLevelAwait from 'vite-plugin-top-level-await'
 import tailwindcss from '@tailwindcss/vite'
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait(), tailwindcss()],
+  plugins: [react(), wasm(), tailwindcss()],
   base: './',
   build: {
     sourcemap: true,
+  },
+  server: {
+    open: true,
   },
 })
